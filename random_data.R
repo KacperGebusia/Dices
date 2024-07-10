29c4b41f94389bfc5236db735a4dce885163b00f
KacperGebusia <gebusiakacper@gmail.com>
2024-07-10 12:12:39 +0200

    feat: data generating

--- random_data.R
+++ random_data.R
@@ -1,2 +1,18 @@
-n = 10
-k = 10
+
+n = 1000
+k = 3
+
+set.seed(123)
+
+
+results = data.frame(
+  Throw = seq(1:n)
+)
+
+
+
+for (i in 1:k) {
+  column_name = paste("Dice", i, sep = "_")
+  results[[column_name]] = sample(1:6, n, replace = TRUE)
+}
+
